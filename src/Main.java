708c1358b7b4192d6207ac462a9d367ff6bde13a
abdullah.enes.karakaya <aenesk.ders.25@gmail.com>
2025-12-17 00:19:51 +0300

    Proje commit

---
+++ src/Main.java
@@ -0,0 +1,146 @@
+// Main.java — Students version
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Main {
+    static final int MONTHS = 12;
+    static final int DAYS = 28;
+    static final int COMMS = 5;
+    static String[] commodities = {"Gold", "Oil", "Silver", "Wheat", "Copper"};
+    static String[] months = {"January","February","March","April","May","June",
+            "July","August","September","October","November","December"};
+
+    public static int[][][] profitData = new int[12][28][5];
+
+    // ======== REQUIRED METHOD LOAD DATA (Students fill this) ========
+    public static void loadData() {
+        Scanner reader = null;
+        for (int Month=0 ; Month<=11 ; Month++){
+            String fileName = "Data_Files/" + months[Month] +".txt";
+            try {
+                // To see if file appears or not
+                File file = new File(fileName);
+                if (!file.exists()) {
+                    System.out.println("Dosya bulunamadı: " + fileName);
+                    System.out.println("Aranan konum: " + file.getAbsolutePath());
+                    return;
+                }
+                reader = new Scanner(Paths.get(file.getAbsolutePath()));
+                while (reader.hasNextLine()) {
+                    try {
+                        String[] parts = reader.nextLine().split(",");
+                        int day = Integer.parseInt(parts[0]);
+                        String commodityName = parts[1];
+                        int profit = Integer.parseInt(parts[2]);
+
+                        int dayIndex = day - 1; // for day 1-28 but index 0-27
+
+                        int commodityIndex = -1;
+                        for (int i = 0; i < commodities.length; i++) {
+                            if (commodities[i].equals(commodityName)) {
+                                commodityIndex = i;
+                                break;
+                            }
+                        }
+                        if (commodityIndex != -1 && dayIndex >= 0 && dayIndex < 28) {
+                            profitData[Month][dayIndex][commodityIndex] = profit;
+                        }
+                    }
+                    catch (Exception e){
+                        continue ;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    reader.close();
+                }
+            }
+        }
+    }
+
+    // ======== 10 REQUIRED METHODS (Students fill these) ========
+
+    public static String mostProfitableCommodityInMonth(int month) {
+
+        if(month >= 0 && month <= 11){
+            Scanner reader = null;
+            System.out.println(months[month]);
+            String fileName = "Data_Files/" + months[month] +".txt";
+            try {
+                File F = new File(fileName);
+                reader = new Scanner(Paths.get(F.getAbsolutePath()));
+                int Max = 0;
+                String Commodity = "";
+                while (reader.hasNextLine()) {
+                    String[] Line = reader.nextLine().split(",");
+                    try{
+                        int p = Integer.parseInt(Line[2]);
+                    }
+                    catch (Exception e){
+                        continue ;
+                    }
+                    for(int i=0 ; i<141 ; i++) {
+                        if(Integer.parseInt(Line[2]) > Max){
+                            Max = Integer.parseInt(Line[2]);
+                            Commodity = Line[1];
+                        }
+                    }
+                }
+                return Commodity + " " + Max ;
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (reader != null) {
+                    reader.close();
+                }
+            }
+        }
+
+        return  "INVALID_MONTH";
+    }
+
+    public static int totalProfitOnDay(int month, int day) {
+        return 1234;
+    }
+
+    public static int commodityProfitInRange(String commodity, int from, int to) {
+        return 1234;
+    }
+
+    public static int bestDayOfMonth(int month) {
+        return 1234;
+    }
+
+    public static String bestMonthForCommodity(String comm) {
+        return "DUMMY";
+    }
+
+    public static int consecutiveLossDays(String comm) {
+        return 1234;
+    }
+
+    public static int daysAboveThreshold(String comm, int threshold) {
+        return 1234;
+    }
+
+    public static int biggestDailySwing(int month) {
+        return 1234;
+    }
+
+    public static String compareTwoCommodities(String c1, String c2) {
+        return "DUMMY is better by 1234";
+    }
+
+    public static String bestWeekOfMonth(int month) {
+        return "DUMMY";
+    }
+
+    public static void main(String[] args) {
+        loadData();
+        System.out.println(mostProfitableCommodityInMonth(0));
+        System.out.println("Data loaded – ready for queries");
+    }
+}
