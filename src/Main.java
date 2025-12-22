6e7154384e0c68cbbd4515215168fc57c5f7e377
abdullah.enes.karakaya <aenesk.ders.25@gmail.com>
2025-12-22 11:19:06 +0300

    Proje Commit - Last

--- src/Main.java
+++ src/Main.java
@@ -265,8 +265,13 @@ public class Main {
                 c2total += profitData[i][j-1][c2Index];
             }
         }
-        if(c1total > c2total) {return c1 +" is better by " + c2;}
-        if(c1total < c2total) {return c2 +" is better by " + c1;}
+
+        if(c1total > c2total) {
+            int dif = c1total - c2total ;
+            return c1 +" is better by " + dif;}
+        if(c1total < c2total) {
+            int dif = c2total - c1total ;
+            return c2 +" is better by " + dif;}
         return "Equal" ;
     }
 
@@ -319,17 +324,17 @@ public class Main {
         loadData();
         System.out.println("Data loaded – ready for queries");
 
-        //System.out.println(mostProfitableCommodityInMonth(0)); //Works but must fix it
-        //System.out.println(profitData[0][0][0]); //Works :)
-        //System.out.println(totalProfitOnDay(0,15)); //Works :)
-        //System.out.println(commodityProfitInRange("Oil",1, 2)); //Works :)
-        //System.out.println(bestDayOfMonth(0)); //Works :)
-        //System.out.println(bestMonthForCommodity("Gold")); //Works :)
-        //System.out.println(consecutiveLossDays("Gold")); //Works :)
-        //System.out.println(daysAboveThreshold("Gold",3000)); //Works :)
-        //System.out.println(biggestDailySwing(0)); //Works :)
-        //System.out.println(compareTwoCommodities("Gold", "Oil")); //Works :)
-        //System.out.println(bestWeekOfMonth(0)); //Works :)
+        System.out.println(mostProfitableCommodityInMonth(0)); //Works but must fix it
+        System.out.println(profitData[0][0][0]); //Works :)
+        System.out.println(totalProfitOnDay(0,15)); //Works :)
+        System.out.println(commodityProfitInRange("Oil",1, 2)); //Works :)
+        System.out.println(bestDayOfMonth(0)); //Works :)
+        System.out.println(bestMonthForCommodity("Gold")); //Works :)
+        System.out.println(consecutiveLossDays("Gold")); //Works :)
+        System.out.println(daysAboveThreshold("Gold",3000)); //Works :)
+        System.out.println(biggestDailySwing(0)); //Works :)
+        System.out.println(compareTwoCommodities("Gold", "Oil")); //Works :)
+        System.out.println(bestWeekOfMonth(0)); //Works :)
         // Don't forget to commit and delete these
     }
 }
